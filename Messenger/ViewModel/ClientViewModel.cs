c442ca2f35b1ecdd05a2e1175a0dd98ca29c68e1
Battle Fill <egorkuznecov283@gmail.com>
2024-05-17 23:24:14 +0300

    Добавьте файлы проекта.

---
+++ Messenger/ViewModel/ClientViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Messenger.View;
+using Messenger.ViewModel.Helpers;
+using Messenger.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Messenger.ViewModel
+{
+    internal class ClientViewModel : BindingHelper
+    {
+        public BindableCommand back { get; set; }
+        public void backk1()
+        {
+            MainWindow second_window = new MainWindow();
+            second_window.Show();
+            Application.Current.Windows[0].Close();
+        }
+
+        public ClientViewModel()
+        {
+
+            back = new BindableCommand(_ => backk1());
+
+
+        }
+    }
+}
---
+++ Messenger/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Messenger.View;
+using Messenger.ViewModel.Helpers;
+using Messenger.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Messenger.ViewModel
+{
+    internal class MainViewModel : BindingHelper
+    {
+        public BindableCommand Server { get; set; }
+        public BindableCommand Client { get; set; }
+        public BindableCommand Nul { get; set; }
+
+
+        public void Serverr()
+        {
+            ServerWindow second_window = new ServerWindow();
+            second_window.Show();
+            Application.Current.MainWindow.Close();
+
+        }
+        public void Clientt()
+        {
+            ClientWindow second_window = new ClientWindow();
+            second_window.Show();
+            Application.Current.MainWindow.Close();
+
+        }
+        public void Nulll()
+        {
+
+            Application.Current.MainWindow.Close();
+
+        }
+        public MainViewModel()
+        {
+
+            Server = new BindableCommand(_ => Serverr());
+            Client = new BindableCommand(_ => Clientt());
+            Nul = new BindableCommand(_ => Nulll());
+
+        }
+        
+
+    }
+}
